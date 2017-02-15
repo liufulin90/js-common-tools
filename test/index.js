ec0c7f0f70268e4a196d565f1bc03f4c1e9d5ec2
liufl0320 <liufl0320@thundersoft.com>
2017-02-15 19:07:09 +0800

    modify array tools

---
+++ test/index.js
@@ -0,0 +1,20 @@
+/**
+ * Created by liufulin on 17-2-15.
+ */
+import { ArrayUnique } from '../lib/'
+
+
+var arr = [5, 3, [8, 8, {b: 1}], 55, [8, 8, {b: 1}], {a: 1}, 3, {a: 1}, 77, 55, 99];
+ console.log(arr)
+ console.log(ArrayUnique(arr))
+
+
+/*var arr = [5, 3, 55, 3,77, 55, 99, 2, 4];
+ console.log(arr);
+ console.log(ArrayQuickSort(arr))*/
+
+/*
+ var arr = [5, 3, 55, 3,77, 55, 99, 2, 4];
+ console.log(arr);
+ console.log(ArrayTools.ArrayQuickSort(arr))
+ */
