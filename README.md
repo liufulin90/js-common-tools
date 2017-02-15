67042eca67139f981a2517b8cbf0f3f5731642b9
liufulin90 <liufulin90@163.com>
2017-02-15 23:43:33 +0800

    update tools and publish to npm

--- README.md
+++ README.md
@@ -4,15 +4,17 @@
 
 ## Functions
 ### Array
-- ArrayUnique 数组去重
-- ArrayQuickSort 数组排序（快排）
+- ArrayUnique 数组去重  eg: arr.ArrayUnique() or ArrayUnique(arr)
+- ArrayQuickSort 数组排序（快排） eg: arr.ArrayQuickSort() or ArrayQuickSort(arr)
 - isNullOrEmpty
-- isArray 判断是否为数组
+- isArray 判断是否为数组 eg: isArray(value)
 - getRandomInt 获得指定区间的随机整数
 - toFixedDecimal
 - getUniqueId
 - getSafeUniqueId
-- trim
+- trimAll 清除字符中所有空格 str.trimAll()
+- trimL 清除字符中左边空格 str.trimL()
+- trimR 清除字符中右边空格 str.trimR()
 
 ## Installation
 
--- test/index.js
+++ test/index.js
@@ -9,9 +9,9 @@ import {
 } from '../lib/'
 
 
-/*var arr = [5, 3, [8, 8, {b: 1}], 55, [8, 8, {b: 1}], {a: 1}, 3, {a: 1}, 77, 55, 99];
+var arr = [5, 3, [8, 8, {b: 1}], 55, [8, 8, {b: 1}], {a: 1}, 3, {a: 1}, 77, 55, 99];
 console.log(arr)
-console.log(ArrayUnique(arr))*/
+console.log(ArrayUnique(arr))
 
 /*var arr = [5, 3, 55, 3,77, 55, 99, 2, 4];
  console.log(arr);
@@ -25,6 +25,7 @@ console.log(ArrayUnique(arr))*/
 
 // console.log(getRandomInt())
 
+/*
 var str = '  asdf   jjj  ';
 console.log(str)
-console.log(str.trimL())
+console.log(str.trimL())*/
