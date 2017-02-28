d0f64cedcc7378e3601ea6f1f8a242214d115ca3
liufulin90 <liufulin90@163.com>
2017-02-28 22:37:21 +0800

    update export *

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ import {getLocalStorage, setLocalStorage, getSessionStorage, setSessionStorage} 
  * include all function to jsct object
  * @type {{arrayUnique: arrayUnique, arrayQuickSort: arrayQuickSort, isArray: isArray, inArray: inArray, getRandomInt: getRandomInt, toFixedDecimal: toFixedDecimal, getUUID: getUUID, isNullOrEmpty: isNullOrEmpty, isString: isString, isCreditCard: isCreditCard, isEmail: isEmail, isUrl: isUrl, isIdentityCard: isIdentityCard, sprintf: sprintf, cookie: cookie, getLocalStorage: getLocalStorage, setLocalStorage: setLocalStorage, getSessionStorage: getSessionStorage, setSessionStorage: setSessionStorage}}
  */
+/*
 const JSCT = {
   arrayUnique, arrayQuickSort, isArray, inArray,
   getRandomInt, toFixedDecimal,
@@ -24,5 +25,12 @@ const JSCT = {
   cookie,
   getLocalStorage, setLocalStorage, getSessionStorage, setSessionStorage
 }
+*/
 
-export default JSCT
+export {
+  arrayUnique, arrayQuickSort, isArray, inArray,
+  getRandomInt, toFixedDecimal,
+  getUUID, isNullOrEmpty, isString, isCreditCard, isEmail, isUrl, isIdentityCard, sprintf,
+  cookie,
+  getLocalStorage, setLocalStorage, getSessionStorage, setSessionStorage
+}
