d8b7ce90f13f79ed7ae382b9f3370c9b53f991ba
liufulin90 <liufulin90@163.com>
2017-03-02 23:12:49 +0800

    update version

--- src/index.js
+++ src/index.js
@@ -16,17 +16,16 @@ import {getLocalStorage, setLocalStorage, getSessionStorage, setSessionStorage} 
 import {Timer} from './date/'
 /**
  * include all function to jsct object
- * @type {{arrayUnique: arrayUnique, arrayQuickSort: arrayQuickSort, isArray: isArray, inArray: inArray, getRandomInt: getRandomInt, toFixedDecimal: toFixedDecimal, getUUID: getUUID, isNullOrEmpty: isNullOrEmpty, isString: isString, isCreditCard: isCreditCard, isEmail: isEmail, isUrl: isUrl, isIdentityCard: isIdentityCard, sprintf: sprintf, cookie: cookie, getLocalStorage: getLocalStorage, setLocalStorage: setLocalStorage, getSessionStorage: getSessionStorage, setSessionStorage: setSessionStorage}}
+ * @type {{arrayUnique: arrayUnique, arrayQuickSort: arrayQuickSort, isArray: isArray, inArray: inArray, getRandomInt: getRandomInt, toFixedDecimal: toFixedDecimal, getUUID: getUUID, isNullOrEmpty: isNullOrEmpty, isString: isString, isCreditCard: isCreditCard, isEmail: isEmail, isUrl: isUrl, isIdentityCard: isIdentityCard, sprintf: sprintf, cookie: cookie, getLocalStorage: getLocalStorage, setLocalStorage: setLocalStorage, getSessionStorage: getSessionStorage, setSessionStorage: setSessionStorage, Timer: Timer}}
  */
-/*
-const JSCT = {
-  arrayUnique, arrayQuickSort, isArray, inArray,
-  getRandomInt, toFixedDecimal,
-  getUUID, isNullOrEmpty, isString, isCreditCard, isEmail, isUrl, isIdentityCard, sprintf,
-  cookie,
-  getLocalStorage, setLocalStorage, getSessionStorage, setSessionStorage
-}
-*/
+// const JSCT = {
+//   arrayUnique, arrayQuickSort, isArray, inArray,
+//   getRandomInt, toFixedDecimal,
+//   getUUID, isNullOrEmpty, isString, isCreditCard, isEmail, isUrl, isIdentityCard, sprintf,
+//   cookie,
+//   getLocalStorage, setLocalStorage, getSessionStorage, setSessionStorage,
+//   Timer
+// }
 
 export {
   arrayUnique, arrayQuickSort, isArray, inArray,
